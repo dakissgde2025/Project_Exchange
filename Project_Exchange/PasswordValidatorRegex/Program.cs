0751aeade04e39706232b316288d1a10545dab16
RGabor821 <r.gabor95@gmail.com>
2025-10-25 23:02:45 +0200

    normal code with regex

---
+++ Project_Exchange/PasswordValidatorRegex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class PasswordValidatorRegex 
+{
+    public const string PasswordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d\s]).{8,}$";
+
+    [Required(ErrorMessage = "Jelszó megadása kötelező.")]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "A jelszónak minimum 8 karakterböl kell állnia.")]
+    [RegularExpression(PasswordPattern, 
+        ErrorMessage = "A jelszónak tartalmaznia kell legalább egy nagybetűt, egy kisbetűt, egy számot és egy speciális karaktert.")]
+    public string Password { get; set; } = string.Empty;
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "A jelszavaknak meg kell egyezniük.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
